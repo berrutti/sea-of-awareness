7779ed9516785a8ca443ffff7bd5d7581d6e0c5b
Matias Berrutti <berrutti@gmail.com>
2020-08-24 20:49:49 -0300

    Update next to 9.3.5. Update react to 16.13.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "rbx/index.css";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preload" href="https://fonts.googleapis.com/css?family=Lato&display=swap" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
